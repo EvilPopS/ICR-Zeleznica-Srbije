540db67b6ed6cec7515553f7b0576d2cc94ef83b
EvilPopS <strahinjapopovic.evilpops@gmail.com>
2022-06-13 20:28:38 +0200

    - Implementiran pregled reda voznje kod obicnog korisnika

---
+++ ZeleznicaSrbije/ZeleznicaSrbije/API/DTOs/UserTimeTableShowDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ZeleznicaSrbije.API.Models;
+
+namespace ZeleznicaSrbije.API.DTOs {
+    internal class UserTimeTableShowDTO {
+        public string TrainNumber { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public TimeSpan TravelTime { get; set; }
+
+        public UserTimeTableShowDTO(Train train, TimeSpan startTime, TimeSpan endTime, TimeSpan travelTime) {
+            TrainNumber = train.TrainNumber;
+            StartTime = startTime;
+            EndTime = endTime;
+            TravelTime = travelTime;
+        }
+    }
+}
--- ZeleznicaSrbije/ZeleznicaSrbije/RegularUserPages/TimetablePage.xaml.cs
+++ ZeleznicaSrbije/ZeleznicaSrbije/RegularUserPages/TimetablePage.xaml.cs
@@ -1,14 +1,32 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
+using ZeleznicaSrbije.API.DTOs;
+using ZeleznicaSrbije.API.Services;
 
 namespace ZeleznicaSrbije.RegularUserPages {
     public partial class TimetablePage : Page {
+        private readonly TrainLineService _trainLineService;
+
+        ObservableCollection<UserTimeTableShowDTO> DTOsList { get; set; }
+
         public TimetablePage() {
             InitializeComponent();
+            _trainLineService = new TrainLineService();
         }
 
-
         private void Search_Click(object sender, RoutedEventArgs e) {
+            string startPlaceVal = ((ComboBoxItem)StartPlaceBox.SelectedItem).Content.ToString();
+            string endPlaceVal = ((ComboBoxItem)EndPlaceBox.SelectedItem).Content.ToString();
+
+            if (startPlaceVal == "" || endPlaceVal == "" || startPlaceVal == endPlaceVal) { 
+            }
+
+            DTOsList = new ObservableCollection<UserTimeTableShowDTO>(
+                _trainLineService.GetTrainLinesDTOs(startPlaceVal, endPlaceVal)
+            );
+            TimeTablesList.ItemsSource = DTOsList;
+
 
         }
     }
